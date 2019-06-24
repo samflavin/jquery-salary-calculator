961bba58963e7a2b09508603e1911881af8d0ba1
Sam Flavin <flavi022@d.umn.edu>
2019-06-24 00:29:22 -0500

    fixed delete!

--- client.js
+++ client.js
@@ -1,24 +1,24 @@
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 let employeeInfo = [];
 let totalSalary = 0;
 
 
 
-function addEmployee () {
+function addEmployee() {
     // capture employee ino
     // place in new object
     let newEmployee = {
-        firstName: $( '#firstNameIn' ).val(),
-        lastName: $( '#lastNameIn' ).val(),
-        employeeId: $( '#idIn').val(),  //returning 
-        jobTitle: $( '#jobTitleIn' ).val(),
-        salary: $( '#salaryIn' ).val()
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        employeeId: $('#idIn').val(),  //returning 
+        jobTitle: $('#jobTitleIn').val(),
+        salary: $('#salaryIn').val()
     }
     console.log(newEmployee);
     //push object into an array
-    employeeInfo.push( newEmployee );
+    employeeInfo.push(newEmployee);
     // display employee info
     displayEmployeeInfo();
     addCosts(newEmployee.salary);
@@ -31,60 +31,65 @@ function addEmployee () {
     $('#salaryIn').val('');
 }
 
-function addCosts (salary) {
-    console.log( 'in add costs' );
-    let ell = $( '#costOut' );
+function addCosts(salary) {
+    console.log('in add costs');
+    let ell = $('#costOut');
     //empty element
     ell.empty();
     salary = Number(salary);
-    totalSalary += (salary/12);
+    totalSalary += (salary / 12);
     //  add up employees salaries
-   
-        ell.append(`Total Monthly Cost: $${totalSalary}`)
-    }
+
+    ell.append(`Total Monthly Cost: $${totalSalary}`)
+}
 
 
 
 function onReady() {
-    $( '#addEmployee' ).on( 'click', addEmployee);
-    $( '#employeeOut' ).on('click', '.deleteEmployee', fireEmployee);
+    $('#addEmployee').on('click', addEmployee);
+    $('#employeeOut').on('click', '.deleteEmployee', fireEmployee);
 }
 
 function displayEmployeeInfo() {
-    console.log( 'in displayEmployeeInfo')
-    let el = $( '.employeeOut' );
+    console.log('in displayEmployeeInfo')
+    let el = $('#employeeOut');
     //empty element
     el.empty();
     //loop through employeeInfo array
-    for (let i=0; i<employeeInfo.length; i++){
+    for (let i = 0; i < employeeInfo.length; i++) {
         //append each item to DOM
-        el.append(`<li>${employeeInfo[i].firstName} ${employeeInfo[i].lastName} ${employeeInfo[i].employeeId} 
-    ${employeeInfo[i].jobTitle} ${employeeInfo[i].salary} <button class="deleteEmployee">Delete</button></li>`)
+        el.append(`<tr>
+        <td>${employeeInfo[i].firstName}</td>
+        <td>${employeeInfo[i].lastName}</td>
+        <td>${employeeInfo[i].employeeId}</td>
+        <td>${employeeInfo[i].jobTitle}</td>
+        <td>${employeeInfo[i].salary}</td>
+        <td><button class="deleteEmployee">Delete</button></td>
+        </tr>`)
     }// end for   
-    
+
 }
 
-function fireEmployee (event) {
+function fireEmployee() {
     console.log('Were gonna have to let you go...', $(this).parent().text());
-    $(this).parent().remove();
+    $(this).closest("tr").remove();
     // Loop employeeInfo to remove selected itemfrom array
-    
 }
 
-function costCheck () {
+function costCheck() {
     console.log('in costCheck function');
     //get value of total cost and check that it's less than 20,000
     let currentCost = totalSalary;
-    if ( currentCost > 20000 ){
+    if (currentCost > 20000) {
         console.log('ABORT MISSION!')
         // Change text color
         changeColor();
-        
+
     }
 
 }
 
-function changeColor (){
-    let elem = $( '#costOut' );
+function changeColor() {
+    let elem = $('#costOut');
     elem.addClass('red');
 }
